1f1fc56f5d9af3109376ee9ed4beaf27d1b15625
NIAN_QIAO <2262793760@qq.com>
2022-11-27 14:41:39 +0800

    add 202isHappy.cpp

---
+++ hash/Happy number/202isHappy.cpp
@@ -0,0 +1,37 @@
+#include <unordered_map>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+using namespace std;
+class Solution
+{
+public:
+    int getSum(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n)
+    {
+        int sum = 0;
+        unordered_set<int> set;
+        while (true)
+        {
+            sum = getsum(n);
+            if (sum == 1)
+                return true;
+            if (set.find(sum) != set.end())
+                return false;
+            else
+                set.insert(sum);
+            n = sum;
+        }
+    }
+};
