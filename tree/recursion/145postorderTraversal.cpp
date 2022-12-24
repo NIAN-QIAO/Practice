9ad68865f5e097816dcebc61e35140427253918d
NIAN_QIAO <2262793760@qq.com>
2022-12-24 15:25:33 +0800

    add tree recursion

---
+++ tree/recursion/145postorderTraversal.cpp
@@ -0,0 +1,34 @@
+#include <stack>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{ // 后序遍历 左右中
+public:
+    void traversal(TreeNode *cur, vector<int> &vec)
+    {
+        if (cur == NULL)
+            return;
+        traversal(cur->left, vec);
+        traversal(cur->right, vec);
+        vec.push_back(cur->val);
+    }
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
---
+++ tree/recursion/94inorderTraversal.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{ // 二叉树的中序遍历：左中右
+public:
+    void traversal(TreeNode *cur, vector<int> &vec)
+    {
+        if (cur == NULL)
+            return;
+
+        traversal(cur->left, vec);
+        vec.push_back(cur->val);
+        traversal(cur->right, vec);
+    }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
