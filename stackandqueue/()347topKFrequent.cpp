0fae98c746471532f79db94183c8f017125ca90d
NIAN_QIAO <2262793760@qq.com>
2022-12-24 14:04:32 +0800

    add stackandqueue

---
+++ stackandqueue/()347topKFrequent.cpp
@@ -0,0 +1,15 @@
+#include <stack>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+// 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+    }
+};
